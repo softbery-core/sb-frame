eec5d327835f2aa0702814b5bb6ddc413d03de74
Softbery Group <softbery@hotmail.com>
2021-05-23 17:50:37 +0200

    Starting files

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ *  File	 : init.php
+ *  Author	 : Paweł Tobis
+ *  Copyright: 2021 by Paweł Tobis
+ *	Website	 : http://www.softbery.org/
+ * 			   http://www.softbery.ddns.net/
+ *  
+ *  Creative Commons Corporation (“Creative Commons”) 
+ *  is not a law firm and does not provide legal services
+ *  or legal advice. Distribution of Creative Commons 
+ *  public licenses does not create a lawyer-client or other
+ *  relationship. Creative Commons makes its licenses and 
+ *  related information available on an “as-is” basis. 
+ *  
+ *    http://creativecommons.org/licenses/by/4.0
+ *  
+ *  Creative Commons gives no warranties regarding its licenses,
+ *  any material licensed under their terms and conditions, or 
+ *  any related information. Creative Commons disclaims all 
+ *  liability for damages resulting from their use to the 
+ *  fullest extent possible.
+ *
+ */
+include_once 'config/config.php';
+/*
+	$controller = scandir(__DIR__.'/src/Controller/');
+    $controlers = array();
+    $j = 0;
+    for ($i = 2; $i < count($controller); $i++) {
+        $value = explode('Controller.php',$controller[$i]);
+        $controllers[$j] = lcfirst($value[0]);
+        $j = $j+1;
+    }
+    
+    return $controllers;*/
